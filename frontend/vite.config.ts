28073181e2b929b68a0c99f63e06b3c1eeb2d99a
ngochai147 <nguyenhuynhngochai147@gmail.com>
2026-05-03 16:39:31 +0700

    a

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    proxy: {
-      '/api/users': {
-        target: 'http://192.168.1.153:8081',
-        changeOrigin: true
-      },
-      '/foods': {
-        target: 'http://192.168.1.62:8082',
-        changeOrigin: true
-      },
-      '/orders': {
-        target: 'http://192.168.1.82:8083',
-        changeOrigin: true
-      },
-      '/payments': {
-        target: 'http://192.168.1.79:8084',
-        changeOrigin: true
-      }
-    }
-  }
+  plugins: [react()],
 })
